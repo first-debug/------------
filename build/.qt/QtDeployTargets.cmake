8732d4ec9a43e71cd456ce2cfc153b6f52902098
vladi <vladislavvasilev53@gmail.com>
2025-05-25 13:15:51 +0300

    добавил скрипты для запуска

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyQtApp_FILE D:/2_sem/cours_c++/build/MyQtApp.exe)
-set(__QT_DEPLOY_TARGET_MyQtApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyQtApp_RUNTIME_DLLS C:/Users/dimov/msys/ucrt64/bin/Qt6Widgets.dll;C:/Users/dimov/msys/ucrt64/bin/Qt6Network.dll;C:/Users/dimov/msys/ucrt64/bin/Qt6Gui.dll;C:/Users/dimov/msys/ucrt64/bin/Qt6Core.dll)
